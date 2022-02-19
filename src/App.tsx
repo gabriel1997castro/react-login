211a031c8e4f2b10dd23d03a67227d308b80f242
Gabriel <gabriel1997.castro@gmail.com>
2022-02-19 10:36:33 -0300

    feat: add font roboto from google and global style

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <div className="App">
+    <>
      <h1>Hello world!</h1>
-    </div>
+     <GlobalStyle />
+    </>
   );
 }
 
