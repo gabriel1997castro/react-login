599fa8349d837848f25da9cd3c5f4ba4d4fdfcea
Gabriel <gabriel1997.castro@gmail.com>
2022-02-19 11:48:04 -0300

    feat: add html from login page

---
+++ src/assets/index.ts
@@ -0,0 +1,9 @@
+import forgotPassword from "./forgotPassword.svg";
+import login from "./login.svg";
+import undrawOnlinePayments from "./undrawOnlinePayments.svg";
+const assets = {
+  forgotPassword,
+  login,
+  undrawOnlinePayments
+};
+export default assets;
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,10 +1,21 @@
 import { Box, Container } from "./styles";
+import assets from '../../assets';
 
 export function Login() {
   return (
     <Container>
       <Box>
+        <img src={assets.login} alt="Login" />
         <h1>login</h1>
+        <input placeholder="example@email.com" type="email"/>
+        <input placeholder="password" type="password"/>
+        <button type="button">Forgot Password?</button>
+        <button type="submit">LOG IN</button>
+        <span>Or connect with social</span>
+        <div>
+        <button type="button">Facebook</button>
+        <button type="button">Google</button>
+        </div>
       </Box>
     </Container>
   )
