506a5f061f5c0b07e507753079c9fe7bdda2fc50
Gabriel <gabriel1997.castro@gmail.com>
2022-02-20 16:26:27 -0300

    feat: add the components and main handlers from upgrade page

--- src/pages/Upgrade/index.tsx
+++ src/pages/Upgrade/index.tsx
@@ -1,5 +1,78 @@
+import { ChangeEvent, useState } from "react";
+import assets from "../../assets";
+import { Box, Button, Image, InputGroup, InputText, Title } from "../../components";
+import { Container, InternalContainer, MonthAndCvvWrapper } from "./styles";
+
 export function Upgrade() {
+  const [creditCardNumber, setCreditCardNumber] = useState("");
+  const [creditCardMonth, setCreditCardMonth] = useState("");
+  const [creditCardCvv, setCreditCardCvv] = useState("");
+
+  const handleCreditCardNumber = (e: ChangeEvent<HTMLInputElement>) => {
+    const num = e.target.value.replace(/\D/g, '');
+    if (num.length > 4 && num.length <= 8) setCreditCardNumber(num.replace(/(\d{4})(\d{1,4})/, '$1 $2'));
+    else if (num.length > 8 && num.length <= 12) setCreditCardNumber(num.replace(/(\d{4})(\d{4})(\d{1,4})/, '$1 $2 $3'));
+    else if (num.length > 12 && num.length <= 16) setCreditCardNumber(num.replace(/(\d{4})(\d{4})(\d{4})(\d{1,4})/, '$1 $2 $3 $4'));
+    else if (num.length > 16) return
+    else setCreditCardNumber(num)
+  }
+
+  const handleCreditCardMonth = (e: ChangeEvent<HTMLInputElement>) => {
+    const num = e.target.value.replace(/\D/g, '');
+    if (num.length > 2 && num.length <= 8) setCreditCardMonth(num.replace(/(\d{2})(\d{1,4})/, '$1/$2'));
+    else if (num.length > 6) return
+    else setCreditCardMonth(num)
+  }
+
+  const handleCvvChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if(e.target.value.length <= 3) setCreditCardCvv(e.target.value)
+  }
+
   return (
-    <div>Upgrade</div>
+    <Container>
+      <Box>
+        <form>
+          <InternalContainer>
+            <Title>Get Pro Account</Title>
+            <Image src={assets.undrawOnlinePayments} alt="Upgrade to pro account" />
+            <InputGroup>
+              <InputText
+                value={creditCardNumber}
+                placeholder="Card Number"
+                type="text"
+                onChange={handleCreditCardNumber}
+                required
+              />
+            </InputGroup>
+            <MonthAndCvvWrapper>
+              <InputGroup>
+                <InputText
+                  value={creditCardMonth}
+                  placeholder="MM/YY"
+                  type="text"
+                  onChange={handleCreditCardMonth}
+                  pattern="^(0[1-9]|1[0-2])(\/|-)([0-9]{4})$"
+                  required
+                />
+              </InputGroup>
+              <InputGroup>
+                <InputText
+                 value={creditCardCvv}
+                 placeholder="CVV"
+                 type="number"
+                 onChange={handleCvvChange}
+                 pattern="\d\d\d" 
+                 required
+                />
+              </InputGroup>
+            </MonthAndCvvWrapper>
+            <InputGroup>
+              <InputText placeholder="ZIP/Postal Code" name="number" type="text" required />
+            </InputGroup>
+            <Button style={{ margin: '2rem 0rem' }} backgroundColor="secondary" type="submit">Upgrade</Button>
+          </InternalContainer>
+        </form>
+      </Box>
+    </Container >
   )
 }
---
+++ src/pages/Upgrade/styles.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  padding: 2.5rem 1rem;
+  align-items: center;
+  justify-content: center;
+  overflow: scroll;
+`;
+
+export const MonthAndCvvWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: row;
+`
+
+export const InternalContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 21.5rem;
+  height: 100%;
+  padding: 0.5rem;
+  align-items: center;
+`;
